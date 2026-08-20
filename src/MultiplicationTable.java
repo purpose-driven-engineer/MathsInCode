6c745b68f8621337d114002f86ce5b3f2ee2366f
Ola Popoola <oladipupo.popoola@gmail.com>
2026-08-20 23:00:23 +0100

    added single number multipliccation

--- src/MultiplicationTable.java
+++ src/MultiplicationTable.java
@@ -1,2 +1,18 @@
+import java.util.Scanner;
+
 public class MultiplicationTable {
+
+    public static void main(String[] args){
+        int number, count;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Multiplication Table of (Enter Number): ");
+        number = scanner.nextInt();
+
+        System.out.println("==============");
+        for (count = 1; count <= 12; count++){
+            System.out.println(number + "*" + count + " = " + (number*count));
+        }
+    }
 }
