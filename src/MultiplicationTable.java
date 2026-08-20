ad9bbac8b5015125068f25ae3a7e21b0746b0cbb
Ola Popoola <oladipupo.popoola@gmail.com>
2026-08-20 23:15:03 +0100

    added slolution for 2 numbers

--- src/MultiplicationTable.java
+++ src/MultiplicationTable.java
@@ -2,17 +2,34 @@ import java.util.Scanner;
 
 public class MultiplicationTable {
 
-    public static void main(String[] args){
-        int number, count;
+//    public static void main(String[] args){
+//        int number, count;
+//
+//        Scanner scanner = new Scanner(System.in);
+//
+//        System.out.println("Multiplication Table of (Enter Number): ");
+//        number = scanner.nextInt();
+//
+//        System.out.println("==============");
+//        for (count = 1; count <= 12; count++){
+//            System.out.println(number + "*" + count + " = " + (number*count));
+//        }
+//    }
 
-        Scanner scanner = new Scanner(System.in);
+     public static void main(String[] args){
 
-        System.out.println("Multiplication Table of (Enter Number): ");
-        number = scanner.nextInt();
+         int a, b, c, d;
 
-        System.out.println("==============");
-        for (count = 1; count <= 12; count++){
-            System.out.println(number + "*" + count + " = " + (number*count));
-        }
-    }
+         Scanner scanner = new Scanner(System.in);
+
+         System.out.println("Multiplication Table of Numbers (Enter a range): "); // separated by whitespace
+         a = scanner.nextInt();
+         b = scanner.nextInt();
+
+         for (c = a; c <= b; c++){
+             for (d = 1; d <= 12; d++){
+                 System.out.println(c + "*" + d + " =" + (c*d));
+             }
+         }
+     }
 }
