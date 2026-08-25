84fd4e5bb5536473796f6c44fbced10e98a4fb8f
Ola Popoola <oladipupo.popoola@gmail.com>
2026-08-25 22:55:39 +0100

    added floyd's triangle checker

--- src/FloydTriangle.java
+++ src/FloydTriangle.java
@@ -1,2 +1,23 @@
+import java.util.Scanner;
+
 public class FloydTriangle {
+    public static void main(String[] args){
+        int n, c, d;
+        int num = 1;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the number of rows of Floyd's triangle: ");
+        n = scanner.nextInt();
+
+        System.out.println("Floyd's Triangle: ");
+
+        for(c = 1; c <= n; c++){
+            for(d = 1; d <= c; d++){
+                System.out.println(num + " ");
+                num++;
+            }
+            System.out.println();
+        }
+    }
 }
